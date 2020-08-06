4552d4e9139dd109b12b0fe65498e6129c4c997d
Adoilson Freitas <adoilsonfreitas@gmail.com>
2020-08-06 00:23:41 -0300

    create controllers

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,15 @@
 import express from 'express';
 import ClassController from './controllers/ClassesControllers';
+import ConnectionController from './controllers/ConnectionsController';
 
 const routes = express.Router();
 const classControllers = new ClassController();
+const connectionController = new ConnectionController();
 
+routes.get('/classes', classControllers.index);
 routes.post('/classes', classControllers.create);
+
+routes.get('/connections', connectionController.index);
+routes.post('/connections', connectionController.create);
 
 export default routes;
