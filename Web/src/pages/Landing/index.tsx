1d716a0e0d786c93980c469a75caa38c9c3c9618
Adoilson Freitas <adoilsonfreitas@gmail.com>
2020-08-06 01:42:47 -0300

    conectando backend e listando conexoes

--- Web/src/pages/Landing/index.tsx
+++ Web/src/pages/Landing/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logoImg from '../../assets/images/logo.svg';
 import landingImg from '../../assets/images/landing.svg';
 import studyIcon from '../../assets/images/icons/study.svg';
@@ -6,9 +6,21 @@ import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
 import {Link}  from 'react-router-dom'
 import { LandingPage } from './styles'
+import api from '../../services/api';
 
 
 export default function Landing() {
+  const [ totalConnections, setTotalConnections ] = useState(0);
+
+  useEffect(() => {
+    api.get('connections').then(response => {
+      const { total } = response.data;
+
+      setTotalConnections(total)
+    });
+      
+    }, []);
+
   return (
     <LandingPage>
       <div id="page-landing-content" className="container">
@@ -36,7 +48,7 @@ export default function Landing() {
         </div>
 
         <span className="total-connections">
-          Total de 200 conexões já realizadas
+          Total de {totalConnections} conexões já realizadas
           <img src={purpleHeartIcon} alt="Coração roxo" />
         </span>
       </div>
